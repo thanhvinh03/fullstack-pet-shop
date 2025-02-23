4a3352d0817430a4ed8044293cb00ff49ddc9dc3
Thanh Vinh <144360526+thanhvinh03@users.noreply.github.com>
2025-02-23 10:30:24 +0700

    [feature] init backend (#1)

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import express, { Express, Request, Response , Application } from 'express';
+import dotenv from 'dotenv';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server 222');
+});
+
+app.get('/api/v1', (req: Request, res: Response) => {
+  res.send({name: 'xiu'});
+});
+
+app.get('/api/v1/products', (req: Request, res: Response) => {
+  const products = [
+    {name: 'xiu'},
+    {name: 'thanhvinh'},
+
+  ]
+  res.json(products)
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at https://localhost:${port}`);
+});
